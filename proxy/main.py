662585ebb14c1411a30ff61815bb1780f6318d05
Matthias Kuhn <matthias@opengis.ch>
2023-05-06 09:42:06 +0200

    Improve logging

--- proxy/main.py
+++ proxy/main.py
@@ -1,11 +1,25 @@
 import logging
+import os
 import sys
 
 from aioflask import Flask
 
 from proxy import proxy
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+loglevel_config = os.environ.get("QGIS_PROXY_FLASK_APP_LOG_LEVEL", "info").lower()
+
+if loglevel_config == "error":
+    log_level = logging.ERROR
+elif loglevel_config == "warning":
+    log_level = logging.WARNING
+elif loglevel_config == "debug":
+    log_level = logging.DEBUG
+else:
+    log_level = logging.INFO
+
+logging.basicConfig(
+    stream=sys.stdout, level=log_level, format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 app = Flask(__name__)
 
@@ -13,4 +27,7 @@ app.register_blueprint(proxy)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(
+        host="0.0.0.0",
+        debug=os.environ.get("QGIS_PROXY_FLASK_APP_DEBUG", False) in ["True", "1", 1],
+    )
--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,9 +1,25 @@
 import logging
 import os
+import sys
 
 from aioflask import Blueprint, g, make_response, request
 from arq import create_pool
 from arq.connections import RedisSettings
+
+loglevel_config = os.environ.get("QGIS_PROXY_FLASK_APP_LOG_LEVEL", "info").lower()
+
+if loglevel_config == "error":
+    log_level = logging.ERROR
+elif loglevel_config == "warning":
+    log_level = logging.WARNING
+elif loglevel_config == "debug":
+    log_level = logging.DEBUG
+else:
+    log_level = logging.INFO
+
+logging.basicConfig(
+    stream=sys.stdout, level=log_level, format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 proxy = Blueprint("proxy", __name__)
 
@@ -12,12 +28,12 @@ async def get_redis_connection():
     redis_connection = getattr(g, "_redis_connection", None)
     if redis_connection is None:
         redis_connection = g._redis_connection = await create_pool(
-            RedisSettings(host="redis")
+            RedisSettings(host=os.environ.get("QGIS_PROXY_REDIS_HOST", "redis"))
         )
     return redis_connection
 
 
-timeout = int(os.environ.get("TIMEOUT", 30))  # Timeout in seconds
+timeout = int(os.environ.get("QGIS_PROXY_JOB_TIMEOUT", 30))  # Timeout in seconds
 
 logging.info(f"Timeout {timeout} seconds")
 
